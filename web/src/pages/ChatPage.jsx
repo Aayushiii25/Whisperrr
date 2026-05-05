95da9a43b69441c0577b67cb01ed35479d41744e
Aayushi Dhurandhar <aayushidhurandhar@gmail.com>
2026-05-06 00:25:08 +0530

    fix: correct import path for useMessages hook in ChatPage component

--- web/src/pages/ChatPage.jsx
+++ web/src/pages/ChatPage.jsx
@@ -6,7 +6,7 @@ import { useSocketConnection } from "../hooks/useSocketConnection";
 import { SparklesIcon, MessageSquareIcon, PlusIcon } from "lucide-react";
 
 import { useChats, useGetOrCreateChat } from "../hooks/useChats";
-import { useMessages } from "../hooks/useMessages";
+import { useMessages } from "../hooks/useMessage";
 import { ChatListItem } from "../components/ChatListItem";
 import { ChatHeader } from "../components/ChatHeader";
 import { MessageBubble } from "../components/MessageBubble";
